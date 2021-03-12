4bc4d09fb27aef7dad74c707f69a36d05d81a9dd
radoneje <den.shevchenko@gmail.com>
2021-03-12 20:22:02 +0300

    fff

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+import { PassThrough } from 'stream';
+var QRCode = require('qrcode')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendStatus(400);
 });
 
 router.get('/qrcode/:userid', async (req, res, next)=>{
-
-  var QRCode = require('qrcode')
   var dataurl=await QRCode.toDataURL(JSON.stringify({userid:req.params.userid}), { errorCorrectionLevel: 'Q', width:300 });
   res.set('Content-Type', 'text/html');
   res.send(Buffer.from('<html><body><img src=\"'+dataurl+'\"/></body></html>'));
+})
+router.get('/qrcodeimage/:userid', async (req, res, next)=>{
+  const qrStream = new PassThrough();
+  var result=await QRCode.toFileStream(JSON.stringify({userid:req.params.userid}), { errorCorrectionLevel: 'Q', width:300 });
+  qrStream.pipe(res);
 })
 module.exports = router;
